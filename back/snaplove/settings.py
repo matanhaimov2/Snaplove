2c4d2ff4ef91c85457555b9047f68ffa2a499522
unknown <matanhaimov2@gmail.com>
2024-12-22 10:40:03 +0200

    backend structure has changed

--- back/snaplove/settings.py
+++ back/snaplove/settings.py
@@ -21,6 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
+# Secret key and other sensitive configurations
 SECRET_KEY = config('SECRET_KEY')
 # - If True => local db applyed, If False - server db (AIVEN) applyed
 USE_LOCAL_DB = config('USE_LOCAL_DB', default=False, cast=bool)
@@ -28,13 +29,18 @@ USE_LOCAL_DB = config('USE_LOCAL_DB', default=False, cast=bool)
 USE_LOCAL_CACHE = config('USE_LOCAL_CACHE', default=False, cast=bool)
 NEON_PASSWORD = config('NEON_PASSWORD')
 REDIS_PASSWORD = config('REDIS_PASSWORD')
+
+# CORS settings from .env
 CORS_ALLOWED_ORIGINS = config('CORS_ALLOWED_ORIGINS', default='http://localhost:3000,http://localhost').split(',')
 CSRF_TRUSTED_ORIGINS = config('CSRF_TRUSTED_ORIGINS', default='http://localhost:3000,http://localhost').split(',')
+
+# Allow hosts from environment variable
+ALLOWED_HOSTS = config('ALLOWED_HOSTS', default='localhost,127.0.0.1').split(',')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ALLOWED_HOSTS
 
 # Application definition
 
