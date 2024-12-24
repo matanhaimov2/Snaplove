9d903ee0530f97183b9e57e8929f18f6651b5e42
unknown <matanhaimov2@gmail.com>
2024-12-24 09:44:51 +0200

    working on websockets prod

--- back/interactions/routing.py
+++ back/interactions/routing.py
@@ -2,5 +2,5 @@ from django.urls import re_path
 from .consumers import ChatConsumer
 
 websocket_urlpatterns = [
-    re_path(r'ws/chat/(?P<room_id>\w+)/$', ChatConsumer.as_asgi()),  # Updated to accept room_id dynamically
+    re_path(r'ws/chat/(?P<room_id>\w+)/$', ChatConsumer.as_asgi()),
 ]
